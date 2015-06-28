a65ac28dbac5e5b12d80335a3b4cf694e295e261
perhansson <per.hansson80@gmail.com>
2015-06-28 10:33:35 -0700

    add bin label fnc

--- plotutils.py
+++ plotutils.py
@@ -22,3 +22,13 @@ def getLegend(x1,y1,x2,y2,h1,h2,txt1,txt2,style1,style2):
 def setGraphStyle(gr):
     gr.SetMarkerStyle(20)
     gr.SetMarkerSize(1.0)
+
+
+def setBinLabels(gr,names):
+    h = gr.GetHistogram()
+    print h.GetNbinsX(), ' vs ', gr.GetN()
+    i = 0
+    for p in names:
+        b = h.FindBin(i)
+        h.GetXaxis().SetBinLabel(b, p )
+        i = i + 1
